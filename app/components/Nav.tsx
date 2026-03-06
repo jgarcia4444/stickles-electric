3b3a9d291df47a518583eaeecfad8bdb45c75cd6
Jake Garcia <bballwiz4@gmail.com>
2026-03-06 13:28:08 -0800

    created a nav bar and a hero section. No functionality yet.

---
+++ app/components/Nav.tsx
@@ -0,0 +1,29 @@
+
+export default function Nav() {
+  return (
+    <nav className="bg-white text-black p-4 flex flex-row justify-between">
+      <div>
+        <a href="/">
+          <h1 className="text-xl font-semibold">Stickles Electric</h1>
+        </a>
+      </div>
+      <ul className="flex flex-row gap-4">
+        <li>
+          <a href="/services">Services</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/areas-we-serve">Areas We Serve</a>
+        </li>
+        <li>
+          <a href="/reviews">Reviews</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Nav from "./components/Nav";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Nav />
+        <main className="w-full px-4 py-8">
+          {children}
+        </main>
       </body>
     </html>
   );
