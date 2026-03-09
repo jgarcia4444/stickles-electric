b62c45aa7c48d4772c06aa09c630d3a01d8a5945
Jake Garcia <bballwiz4@gmail.com>
2026-03-08 23:01:28 -0700

    added in a call now button.

---
+++ app/components/CallNowButton.tsx
@@ -0,0 +1,8 @@
+
+export default function CallNowButton() {
+  return (
+    <button className="w-full bg-white text-primary-blue px-6 py-2 rounded-lg text-xl font-semibold hover:scale-105 transition-all hover:cursor-pointer">
+      Call Now
+    </button>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
-import Image from "next/image";
+import CallNowButton from "./components/CallNowButton";
 
 export default function Home() {
   return (
     <div className="">
       <main className="flex flex-row">
-        <div className="w-1/2">
-          <h1 className="text-2xl font-bold">Trusted Electrician Serving Palm Desert and Surrounding Areas</h1>
-          <div className="w-full rounded-full h-1 bg-white my-2" />
-          <h2 className="text-xl font-semibold">Electrician Services</h2>
+        <div className="w-1/2 flex flex-col gap-2 py-4">
+          <h1 className="text-2xl font-bold text-white">Trusted Electrician Serving Palm Desert and Surrounding Areas</h1>
+          <div className="w-full rounded-full h-1 bg-linear-to-r to-white from-primary-blue" />
+          <h2 className="text-xl font-semibold text-white mb-8">Electrician Services</h2>
+          <div className="w-1/2">
+            <CallNowButton />
+          </div>
         </div>
-        <div className="w-1/2">
+        <div className="w-1/2 flex flex-col items-center justify-center">
+          
         </div>
       </main>
     </div>
