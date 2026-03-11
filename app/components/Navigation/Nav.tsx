fd23776ad3a5e2e7915b165d48b03adb020a718d
Jake Garcia <bballwiz4@gmail.com>
2026-03-11 12:10:36 -0700

    Nav links now change when hovered along with icons and the selected nav link is highlighted.

--- app/components/Navigation/Nav.tsx
+++ app/components/Navigation/Nav.tsx
@@ -2,18 +2,27 @@
 import { PiWrenchFill } from "react-icons/pi";
 import { useState } from "react";
 import NavLink from "./NavLink";
+import { usePathname } from "next/navigation";
 
 export default function Nav() {
 
+  const pathname = usePathname();
   const iconSize = 20;
 
   const navLinks = [
-    { href: "/services", text: "Services", icon: <PiWrenchFill size={iconSize} className="text-primary-blue" /> },
-    { href: "/about", text: "About", icon: <PiWrenchFill size={iconSize} className="text-primary-blue" /> },
-    { href: "/areas-we-serve", text: "Areas We Serve", icon: <PiWrenchFill size={iconSize} className="text-primary-blue" /> },
-    { href: "/reviews", text: "Reviews", icon: <PiWrenchFill size={iconSize} className="text-primary-blue" /> },
-    { href: "/contact", text: "Contact", icon: <PiWrenchFill size={iconSize} className="text-primary-blue" /> },
+    { href: "/services", text: "Services", icon: <PiWrenchFill size={iconSize}  /> },
+    { href: "/about", text: "About", icon: <PiWrenchFill size={iconSize} /> },
+    { href: "/areas-we-serve", text: "Areas We Serve", icon: <PiWrenchFill size={iconSize}  /> },
+    { href: "/reviews", text: "Reviews", icon: <PiWrenchFill size={iconSize} /> },
+    { href: "/contact", text: "Contact", icon: <PiWrenchFill size={iconSize} /> },
   ];
+
+  
+
+  const isSelected = (href: string) => {
+    console.log(pathname, href);
+    return pathname === href;
+  };
 
   return (
     <nav className=" p-4 flex flex-row justify-between bg-light-blue">
@@ -22,8 +31,8 @@ export default function Nav() {
           <h1 className="hover:text-secondary-blue text-xl font-semibold text-white">Stickles Electric</h1>
         </a>
       </div>
-      <div className="flex flex-row gap-4">
-        {navLinks.map((link) => <NavLink key={link.href} href={link.href} text={link.text} icon={link.icon} />)}
+      <div className="flex flex-row gap-4 text-primary-blue">
+        {navLinks.map((link) => <NavLink selected={isSelected(link.href)} key={link.href} href={link.href} text={link.text} icon={link.icon} />)}
       </div>
     </nav>
   );
--- app/components/Navigation/NavLink.tsx
+++ app/components/Navigation/NavLink.tsx
@@ -1,10 +1,20 @@
-import { ReactNode } from "react";
 
-export default function NavLink({ href, text, icon }: { href: string; text: string; icon: ReactNode }) {
+"use client";
+
+import { ReactNode } from "react";
+import { useState } from "react";
+
+export default function NavLink({ href, text, icon, selected }: { href: string; text: string; icon: ReactNode; selected: boolean }) {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <a className="flex items-center justify-center gap-1 text-white" href={href}>
-      {icon}
-      <span className="text-primary-blue">{text}</span>
-    </a>
+    <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className="flex flex-col ">
+        <a className="flex items-center justify-center gap-1 text-white hover:text-secondary-blue transition-colors" href={href}>
+            <div className={`text-primary-blue ${!selected ? "text-primary-blue" : "text-white"} ${isHovered ? "text-secondary-blue" : ""} transition-colors`}>
+                {icon}
+            </div>
+        <span className={`${!selected ? "text-primary-blue" : "text-white"} ${isHovered ? "text-secondary-blue" : ""} transition-colors`}>{text}</span>
+        </a>
+        {selected && <div className="rounded-full h-1 w-full bg-white"></div>}
+    </div>
   );
 }
